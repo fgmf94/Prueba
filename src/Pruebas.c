8e83243db0d4fcc0313955080e1b8fa41862f4dd
fgmf94 <f_gmf94@hotmail.com>
2015-11-27 22:38:58 -0300

    first commit

---
+++ src/Pruebas.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : Pruebas.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("\033[1;30mGris\033[0m\n");
+	puts("\033[1;31mRojo\033[0m\n");
+	puts("\033[1;32mVerde\033[0m\n");
+	puts("\033[1;33mAmarillo\033[0m\n");
+	puts("\033[1;34mAzul\033[0m\n");
+	puts("\033[1;35mVioleta\033[0m\n");
+	puts("\033[1;36mCeleste\033[0m\n");
+	puts("\033[1;37mBlanco\033[0m\n");
+	return EXIT_SUCCESS;
+}
